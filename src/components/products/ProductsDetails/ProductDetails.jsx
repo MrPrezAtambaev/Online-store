f060b5401bd1fced48ca2a214f87fcaf79f49c74
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2023-03-06 11:11:59 +0600

    Verstka Cart and Details Added

--- src/components/products/ProductsDetails/ProductDetails.jsx
+++ src/components/products/ProductsDetails/ProductDetails.jsx
@@ -1,55 +1,146 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useProducts } from "../../../context/ProductsContext";
 import { useCart } from "../../../context/CartContextProvider";
+import { Parallax } from "react-parallax";
+import "./ProductDetails.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const ProductDetails = () => {
   const { getOneProduct, oneProduct } = useProducts();
   const { id } = useParams();
   const navigate = useNavigate();
   const { addProductToCart, checkProductInCart } = useCart();
+  const [value, setValue] = useState(1);
 
   useEffect(() => {
     getOneProduct(id);
   }, []);
+
+  const handleOnChange = (event) => {
+    const newValue = event.target.value;
+    if (newValue > 100) {
+      setValue(100);
+    } else if (newValue < 1) {
+      setValue(1);
+    } else {
+      setValue(newValue);
+    }
+  };
   return (
-    <>
-      {oneProduct ? (
-        <div className="product_card">
-          <div className="product_card_sort">
-            <div className="product_card_content">
-              <img
-                src="https://m.media-amazon.com/images/I/51-+O3-wFxL._AC_UF1000,1000_QL80_.jpg"
-                alt=""
-                className="prd_content_img"
-              />
-
-              <span className="prd_content_span">{oneProduct.category}</span>
-              <div className="prd_content_flex">
-                <h5 className="prd_content_title">{oneProduct.title}</h5>
-                <h6 className="prd_content_price">${oneProduct.price}</h6>
+    <Parallax
+      bgImage="https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      bgImageAlt="background image"
+      strength={150}
+      blur={1}
+    >
+      <div className="container my-5">
+        {oneProduct ? (
+          <>
+            <div className="card details-card p-0">
+              <a href="/" className="back-link">
+                <ArrowBackIosIcon />
+              </a>
+              <div className="row">
+                <div className="col-md-6 col-sm-12">
+                  <img
+                    className="img-fluid details-img"
+                    src={oneProduct.img}
+                    alt=""
+                  />
+                </div>
+                <div
+                  className="col-md-6 col-sm-12 description-container p-5"
+                  style={{ color: "black" }}
+                >
+                  <div className="main-description">
+                    <p className="product-category mb-0">{oneProduct.type}</p>
+                    <h3>{oneProduct.title}</h3>
+                    <hr />
+                    <p className="product-price">${oneProduct.price}</p>
+                    <p className="product-price">Color: {oneProduct.color}</p>
+                    <form className="add-inputs" method="post">
+                      <input
+                        type="number"
+                        className="form-control"
+                        id="cart_quantity"
+                        name="cart_quantity"
+                        value={value}
+                        min={1}
+                        max={100}
+                        step={1}
+                        onChange={handleOnChange}
+                      />
+                      <button
+                        name="add_to_cart"
+                        type="submit"
+                        className="btn btn-success btn-lg"
+                        onClick={() => {
+                          addProductToCart(oneProduct);
+                          navigate("/cart");
+                        }}
+                      >
+                        Add to cart
+                      </button>
+                    </form>
+                    <form className="add-inputs" method="post">
+                      <button
+                        name="add_to_cart"
+                        type="submit"
+                        className="btn btn-primary btn-lg"
+                        onClick={() => navigate(`/edit/${oneProduct.id}`)}
+                      >
+                        Edit
+                      </button>
+                    </form>
+                    <div style={{ clear: "both" }}></div>
+                    <hr />
+                    <p className="product-title mt-4 mb-1">
+                      About this product
+                    </p>
+                    <p className="product-description mb-4">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Quis assumenda voluptatem tempore dolor quod. Expedita,
+                      id, minus similique dolor sed adipisci aliquam natus amet
+                      doloremque delectus cupiditate? Sint, quasi, ad
+                      necessitatibus omnis quaerat tenetur corporis porro aut,
+                      natus ex ab id vel odit veniam fugiat temporibus aperiam
+                      quia rem minima!
+                    </p>
+                    <hr />
+                    <p className="product-title mt-4 mb-1">
+                      Share this product
+                    </p>
+                    <ul className="social-list">
+                      <li>
+                        <a href="#">
+                          <FacebookIcon />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <TwitterIcon />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <InstagramIcon />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-
-              <button
-                className="prd_content_btn"
-                onClick={() => addProductToCart(oneProduct)}
-              >
-                Buy Now
-              </button>
-
-              <button
-                onClick={() => navigate(`/edit/${oneProduct.id}`)}
-                className="prd_content_btn_2"
-              >
-                Edit
-              </button>
             </div>
-          </div>
-        </div>
-      ) : (
-        <h1>Loading</h1>
-      )}
-    </>
+          </>
+        ) : (
+          <h1>Loading</h1>
+        )}
+      </div>
+    </Parallax>
   );
 };
 
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -13,11 +13,17 @@ import { useNavigate } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import ClearIcon from "@mui/icons-material/Clear";
 import "./CartPage.css";
 
 export default function CartPage() {
-  const { getCart, cart, changeProductCount, deleteProductFromCart } =
-    useCart();
+  const {
+    getCart,
+    cart,
+    changeProductCount,
+    deleteProductFromCart,
+    cartLength,
+  } = useCart();
 
   const navigate = useNavigate();
 
@@ -31,90 +37,188 @@ export default function CartPage() {
   }
 
   return (
-    <>
-      <h3 style={{ color: "white" }}>Card Page</h3>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Picture</TableCell>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Type</TableCell>
-              <TableCell align="center">Category</TableCell>
-              <TableCell align="center">Color</TableCell>
-              <TableCell align="center">Price</TableCell>
-              <TableCell align="center">Count</TableCell>
-              <TableCell align="center">Sub Price</TableCell>
-              <TableCell align="center">Remove</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {cart?.products.map((row) => (
-              <TableRow
-                key={row.item.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>
-                  <img src={row.item.img} alt="error:(" width="50" />
-                </TableCell>
-                <TableCell align="center">{row.item.title}</TableCell>
-                <TableCell align="center">{row.item.type}</TableCell>
-                <TableCell align="center">{row.item.category}</TableCell>
-                <TableCell align="center">{row.item.color}</TableCell>
-                <TableCell align="center">${row.item.price}</TableCell>
-                <TableCell align="center">
-                  <IconButton
-                    aria-label="reduce"
-                    size="small"
-                    onClick={() =>
-                      changeProductCount(row.count - 1, row.item.id)
-                    }
-                  >
-                    <RemoveIcon fontSize="small" />
-                  </IconButton>
-                  <TextField
-                    type="number"
-                    size="small"
-                    value={row.count}
-                    onChange={(e) =>
-                      changeProductCount(e.target.value, row.item.id)
-                    }
-                  />
-                  <IconButton
-                    aria-label="increase"
-                    size="small"
-                    onClick={() =>
-                      changeProductCount(row.count + 1, row.item.id)
-                    }
-                  >
-                    <AddIcon fontSize="small" />
-                  </IconButton>
-                </TableCell>
-                <TableCell align="center">${row.subPrice}</TableCell>
-                <TableCell align="center">
-                  <button
-                    onClick={() => deleteProductFromCart(row.item.id)}
-                    className="custom-btn btn-9"
-                  >
-                    Delete
-                  </button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <Typography variant="h6" component="div">
-          Total Price: ${cart?.totalPrice}
-          <Button
-            onClick={() => {
-              cartCleaner();
-              navigate("/");
-            }}
-          >
-            BUY NOW
-          </Button>
-        </Typography>
-      </TableContainer>
-    </>
+    <section className="h-100 h-custom" style={{ backgroundColor: "#d2c9ff" }}>
+      <div className="container py-5 h-100 ">
+        <div className="row d-flex justify-content-center align-items-center h-100 w-100">
+          <div className="col-12">
+            <div
+              className="card card-registration card-registration-2"
+              style={{ borderRadius: "15px" }}
+            >
+              <div className="card-body p-0">
+                <div className="row g-0">
+                  <div className="col-lg-8">
+                    <div className="p-4">
+                      <div className="d-flex justify-content-between align-items-center mb-5">
+                        <h1 className="fw-bold mb-0 text-black">
+                          Shopping Cart
+                        </h1>
+                        <h6
+                          className="mb-0 text-muted"
+                          style={{ color: "black" }}
+                        >
+                          {cartLength}
+                        </h6>
+                      </div>
+                      <hr className="my-4" />
+                      {cart?.products.map((row) => (
+                        <div
+                          key={row.item.id}
+                          className="row mb-4 d-flex justify-content-between align-items-center"
+                        >
+                          <div className="col-md-2 col-lg-2 col-xl-2">
+                            <img
+                              src={row.item.img}
+                              className="img-fluid rounded-3"
+                              alt="Cotton T-shirt"
+                            />
+                          </div>
+                          <div className="col-md-3 col-lg-3 col-xl-3">
+                            <h6 className="text-muted">{row.item.title}</h6>
+                            <h6 className="text-black mb-0">
+                              Category: {row.item.category}
+                            </h6>
+                            <h6 className="text-black mb-0">
+                              Color: {row.item.color}
+                            </h6>
+                            <h6 className="text-black mb-0">
+                              Type: {row.item.type}
+                            </h6>
+                          </div>
+                          <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                            <button
+                              className="btn btn-link px-2"
+                              onClick={() =>
+                                changeProductCount(
+                                  parseInt(row.count) - 1,
+                                  row.item.id
+                                )
+                              }
+                            >
+                              <RemoveIcon fontSize="small" />
+                            </button>
+
+                            <input
+                              id="form1"
+                              min="0"
+                              name="quantity"
+                              value={row.count}
+                              type="text"
+                              className="form-control form-control-sm"
+                              onChange={(e) =>
+                                changeProductCount(
+                                  parseInt(e.target.value),
+                                  row.item.id
+                                )
+                              }
+                            />
+
+                            <button
+                              className="btn btn-link px-2"
+                              onClick={() =>
+                                changeProductCount(
+                                  parseInt(row.count) + 1,
+                                  row.item.id
+                                )
+                              }
+                            >
+                              <AddIcon fontSize="small" />
+                            </button>
+                          </div>
+
+                          <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                            <h6 className="mb-0" style={{ color: "black" }}>
+                              ${row.subPrice}
+                            </h6>
+                          </div>
+                          <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                            <ClearIcon
+                              style={{ color: "black" }}
+                              onClick={() => deleteProductFromCart(row.item.id)}
+                            />
+                          </div>
+                        </div>
+                      ))}
+
+                      <hr className="my-4" />
+
+                      <div className="pt-5">
+                        <h6 className="mb-0">
+                          <h6 style={{ color: "black" }}>
+                            Total Price: ${cart?.totalPrice}
+                          </h6>
+                          <a href="/" className="text-body">
+                            <i className="fas fa-long-arrow-alt-left me-2"></i>
+                            Back to shop
+                          </a>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 bg-grey">
+                    <div className="p-1">
+                      <h3
+                        className="fw-bold mb-5 mt-2 pt-1"
+                        style={{ color: "black" }}
+                      >
+                        Summary
+                      </h3>
+                      <hr className="my-4" />
+
+                      <div className="d-flex justify-content-between mb-4">
+                        <h5
+                          className="text-uppercase"
+                          style={{ color: "black" }}
+                        >
+                          Count:
+                        </h5>
+                        <h5 style={{ color: "black" }}>Price:</h5>
+                      </div>
+                      <h5
+                        className="text-uppercase mb-3"
+                        style={{ color: "black" }}
+                      >
+                        Give code
+                      </h5>
+
+                      <div className="mb-5">
+                        <div className="form-outline">
+                          <input
+                            type="text"
+                            id="form3Examplea2"
+                            placeholder="Enter your code"
+                            className="form-control form-control-lg"
+                          />
+                        </div>
+                      </div>
+
+                      <hr className="my-4" />
+
+                      <div className="d-flex justify-content-between mb-5">
+                        <h5
+                          className="text-uppercase"
+                          style={{ color: "black" }}
+                        >
+                          Total price:
+                        </h5>
+                        <h5 style={{ color: "black" }}>Number of products:</h5>
+                      </div>
+
+                      <button
+                        type="button"
+                        className="btn btn-dark btn-block btn-lg"
+                        data-mdb-ripple-color="dark"
+                      >
+                        Checkout
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
